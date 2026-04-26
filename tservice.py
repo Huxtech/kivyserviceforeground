0ea46388460b78d915be606e0b1d17c3e5564f43
Huxtech <godspowerotu@gmail.com>
2026-04-26 21:41:22 +0100

    p4a hook2

--- tservice.py
+++ tservice.py
@@ -15,14 +15,14 @@ PythonService = autoclass('org.kivy.android.PythonService')
 
 
 service = PythonService.mService
-# foreground_type= ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC if BuildVersion.SDK_INT >= 30 else 0
+foreground_type= ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC if BuildVersion.SDK_INT >= 30 else 0
 fmt = lambda s: f"{int(s//3600)}h {int((s%3600)//60)}m {int(s%60)}s"
 
 
 n=Notification(title="Foreground Service Active", message="This service is running in the foreground")
 builder=n.fill_args() # not using .send() allowing .startForeground() to send initial notification
 
-service.startForeground(n.id, builder.build())#, foreground_type)
+service.startForeground(n.id, builder.build(), foreground_type)
 
 print("Foreground Service is alive. Entering main loop...")
 n1 = Notification(title="Running for 0h 0m 0s")
